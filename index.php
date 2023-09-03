fcef29d198fcf221a1d216b60085354e64a76c9f
Oyero Habib <oyerohabib@yahoo.com>
2023-09-03 22:42:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,28 +3,18 @@
 require_once __DIR__ . '/app/Controllers/CapsuleController.php';
 require_once  __DIR__ . '/app/Helpers/function.php';
 
-
-
 $capsuleController = new CapsuleController();
 
-
 if ($_SERVER["REQUEST_METHOD"] == ActionEnum::GET->name && $_SERVER['REQUEST_URI'] == '/api/get_capsules') {
-      $capsuleController->getCapsules();
+    $capsuleController->getCapsules();
 }
+
 $requestPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $pattern = '/^\/api\/get_capsules\/([A-Za-z0-9]+)$/';
 
-if ($_SERVER["REQUEST_METHOD"] == ActionEnum::GET->name && preg_match($pattern, $requestPath, $matches)) {
+$actionEnum = new ActionEnum(); // Instantiate ActionEnum
+
+if ($_SERVER["REQUEST_METHOD"] == $actionEnum->GET->name && preg_match($pattern, $requestPath, $matches)) {
     $serial = $matches[1];
     $capsuleController->getOneCapsule($serial);
 }
-
-//handleRequest('/api/get_capsules', ActionEnum::GET->name, function () use ($capsuleController) {
-//      $capsuleController->getCapsules();
-//});
-//
-//$serial = "";
-//handleRequest('/api/get_capsules/'.  $serial, ActionEnum::GET->name, function () use ($capsuleController) {
-//    global $serial;
-//    $capsuleController->getOneCapsule($serial);
-//});
